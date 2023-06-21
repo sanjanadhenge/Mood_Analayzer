59cf1a9851b8191fc79ac13b40ecb3f0e468a747
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-21 19:53:41 +0530

    [sanjana] Add : Mood Analyze

---
+++ Mood_Analayzer/MoodAnalyze.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analayzer
+{
+    public class MoodAnalyze
+    {
+        public string Analyze(string str)
+        {
+            if(str.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+
+using Mood_Analayzer;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        //TC1.1
+        [Test]
+        public void GivenSadMood_WhenAnalyze_shouldReturnSad()
+        {
+            string message = "I am in Sad Mood";
+            MoodAnalyze moodAnalyzer = new MoodAnalyze();
+            string result1 = moodAnalyzer.Analyze(message);
+
+            Assert.AreEqual("SAD", result1);
+        }
+        //TC1.2
+        [Test]
+        public void GivenAnyMood_WhenAnalyze_shouldReturnSad()
+        {
+            string message = "I am in Any Mood";
+            MoodAnalyze moodAnalyzer = new MoodAnalyze();
+            string result = moodAnalyzer.Analyze(message);
+
+            Assert.AreEqual("HAPPY", result);
+        }
+    }
+}
